2c5b81b398be7f2edcf1a0d1d016f482441086de
Charlotte Pearce <charlotte.pearce1984@gmail.com>
2019-01-17 19:48:33 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,6 +1,11 @@
 array = [1, 2, 3]
+
 def square_array(array)
-    array.each {|i|*2:}
+    array.each { |a| print a *= 2, " " }
     end
+
 new_numbers = [9,10,16,25]
      square_array(new_numbers)
+
+     
+     
