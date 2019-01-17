677a48af9d11913814e0ede33aeb933356305844
Charlotte Pearce <charlotte.pearce1984@gmail.com>
2019-01-17 20:28:29 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,6 +2,6 @@ array = [1, 2, 3]
 
     def square_array(array)
       new_array = []
-      array.each{|a| new_array(a*a)}
+      array.each{|a| new_array.push(a*a)}
       return new_array
     end
