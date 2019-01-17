9caa18799e19885fe43d9ccf119b8700c4bf9107
Charlotte Pearce <charlotte.pearce1984@gmail.com>
2019-01-17 20:29:17 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,7 @@
-def square_array(array)
-  # your code here
-end
+array = [1, 2, 3]
+
+    def square_array(array)
+      new_array = []
+      array.each{|a| new_array.push(a*a)}
+      return new_array
+    end
